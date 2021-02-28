4213ce967e155901e876472d4ed7300c4858ba92
defidevpro <79772575+defidevpro@users.noreply.github.com>
2021-02-28 15:08:57 -0800

    Add files via upload

---
+++ precache-manifest.4ee14d998d8b3de20831a399d3fa2b91.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da734afc8b0e8eb7519e421c24e2f8a8",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "fd12e5d1a63c6df7b4cb",
+    "url": "/dashboard/static/js/2.051507ab.chunk.js"
+  },
+  {
+    "revision": "357559db386166da7d12cf80cd09f99c",
+    "url": "/dashboard/static/js/2.051507ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40683c2b42fa83361bfa",
+    "url": "/dashboard/static/js/3.43f9cfdb.chunk.js"
+  },
+  {
+    "revision": "332e72c5decc890c5fe2",
+    "url": "/dashboard/static/js/main.4f65db36.chunk.js"
+  },
+  {
+    "revision": "8318a68c0ca601ae028d",
+    "url": "/dashboard/static/js/runtime-main.da32b13a.js"
+  },
+  {
+    "revision": "d37a9e04001fc815db0767f5ec3f1987",
+    "url": "/dashboard/static/media/FdoLogo256gold.d37a9e04.png"
+  },
+  {
+    "revision": "b58cf5eb323061da6d9d0af7977c5214",
+    "url": "/dashboard/static/media/HarvestRadio.b58cf5eb.mp3"
+  },
+  {
+    "revision": "575896520020fbd0f4ea1674ceda4b3f",
+    "url": "/dashboard/static/media/eth_dai.57589652.svg"
+  },
+  {
+    "revision": "dfbb5e3792d89e39958c3a61081268df",
+    "url": "/dashboard/static/media/eth_usdc.dfbb5e37.svg"
+  },
+  {
+    "revision": "1bcf41fd64af68e2d586c3a005d32f29",
+    "url": "/dashboard/static/media/eth_usdt.1bcf41fd.svg"
+  },
+  {
+    "revision": "de8940ed6afd4037ee2c397de352c9bb",
+    "url": "/dashboard/static/media/eth_wbtc.de8940ed.svg"
+  },
+  {
+    "revision": "50ba3f462c6db64504cb86cc419e677e",
+    "url": "/dashboard/static/media/gif_tractor.50ba3f46.gif"
+  },
+  {
+    "revision": "03f6920767840988f31f595cc59607be",
+    "url": "/dashboard/static/media/sushi_wbtc.03f69207.svg"
+  }
+]);
